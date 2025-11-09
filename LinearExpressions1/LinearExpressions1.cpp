327591b1e7d1aa4e5e570cd048bf8a86befcdea2
Данило Міхневич <ipz251_mdl@student.ztu.edu.ua>
2025-11-09 17:58:35 +0200

    Виконано завдання 1(робота над математичними виразами).

--- LinearExpressions1/LinearExpressions1.cpp
+++ LinearExpressions1/LinearExpressions1.cpp
@@ -1,20 +1,35 @@
-// LinearExpressions1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <windows.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    double a, b, c, d;
+    double x, y, z, r;
+
+    printf("¬вед≥ть число a: ");
+    scanf("%lf", &a);
+    printf("¬вед≥ть число b: ");
+    scanf("%lf", &b);
+    printf("¬вед≥ть число c: ");
+    scanf("%lf", &c);
+    printf("¬вед≥ть число d: ");
+    scanf("%lf", &d);
+
+    x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) + pow(a, 2) / pow(b, 2);
+    y = (5 * (a + b) * (c - d)) / (0.5 * c) + pow(d, 2) * (pow(a, 2) - pow(b, 2)) / (b - a);
+    z = (pow((pow(x, 2) - 2 * x), 3) - 4 * (pow(x, 4) + 1) * (1 - b)) / (5 * a + 3 * b);
+    r = ((0.5 * a + 0.75 * b - 7.0 / 5.0) / (3 * c + 1)) + 1 / (a - c);
+
+    printf("\n–езультати обчислень:\n");
+    printf("x = %.4lf\n", x);
+    printf("y = %.4lf\n", y);
+    printf("z = %.4lf\n", z);
+    printf("r = %.4lf\n", r);
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
